b436dbaee6b2e0a419cbc4dbbc2815ce03aeedce
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-02-01 23:42:17 +0100

    Added in-memory caching

--- RickAndMortyMs/Program.cs
+++ RickAndMortyMs/Program.cs
@@ -10,6 +10,8 @@ builder.Services
     .AddScoped<ISearchCharacterInEpisodeService, SearchCharacterInEpisodeService>()
     .AddScoped<IRepository, Repository>();
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
